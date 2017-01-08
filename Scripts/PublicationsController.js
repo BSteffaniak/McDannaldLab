30817516a79049dd85749431a2169df3d77ad4f3
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-07 21:55:39 -0800

    Added publication sorting functionality.

--- Scripts/PublicationsController.js
+++ Scripts/PublicationsController.js
@@ -1,16 +1,41 @@
 var publications = [
     {
         header: 'Asdf test',
+        date: '01/20/2016',
         fileUrl: 'Publications/asdf.txt',
         description: '<b>asdfasdfasdfasdfa</b>'
     },
     {
         header: 'Asdf test2',
+        date: '01/07/2017',
         fileUrl: 'Publications/asdf2.txt',
         description: '<b>asdfasdfasdfsadfdfdfdasdfa</b>'
     },
 ];
 
-app.controller("PublicationsController", ["$scope", function ($scope) {
+app.controller("PublicationsController", ["$scope", "$sce", function ($scope, $sce) {
     $scope.publications = publications;
+    $scope.years = [];
+    
+    $scope.publicationsFromYear = function (year) {
+        return $scope.publications.filter(function (d) {
+            return d.year == year;
+        });
+    };
+    
+    $scope.$watch("publications", function (value) {
+        value.forEach(function (pub) {
+            var d = moment(pub.date, "MM/DD/YYYY");
+            
+            var year = d.year();
+            
+            pub.time = +d;
+            pub.year = year;
+            pub.html = $sce.trustAsHtml(pub.description);
+            
+            if ($scope.years.indexOf(year) < 0) {
+                $scope.years.push(year);
+            }
+        });
+    });
 }]);
