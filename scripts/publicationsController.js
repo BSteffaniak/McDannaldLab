4f72e64e4328fc824cdf53940caf62854b262be4
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-09 03:47:26 -0800

    Updated to use real publications.

--- scripts/publicationsController.js
+++ scripts/publicationsController.js
@@ -1,21 +1,48 @@
 var publications = [
     {
-        header: 'Asdf test',
+        header: 'DiLeo A, Wright KM and McDannald MA (2016). "Sub-second fear discrimination in rats: Adult impairment in adolescent heavy alcohol drinkers." Learning & Memory 23: 618-622.',
         date: '01/20/2016',
         fileUrl: 'publications/asdf.txt',
-        description: '<b>asdfasdfasdfasdfa</b>'
     },
     {
-        header: 'Asdf test2',
-        date: '01/07/2017',
+        header: 'Lopatina N, McDannald MA, Steyer CV, Peterson J, Sadacca BF, Cheer JF and Schoenbaum G (2016). "Medial orbitofrontal neurons preferentially signal cues predicting changes in reward during unblocking." Journal of Neuroscience 36(32): 8416-8424.',
+        date: '01/07/2016',
         fileUrl: 'publications/asdf2.txt',
-        description: '<b>asdfasdfasdfsadfdfdfdasdfa</b>'
     },
     {
-        header: 'Something',
-        date: '01/08/2017',
+        header: 'Lopatina N, McDannald MA, Steyer CV, Sadacca BF, Cheer JF and Schoenbaum G (2015). "Lateral orbitofrontal neurons acquire responses to upshifted, downshifted, or blocked cues during Pavlovian unblocking." eLife 10.7554/eLife.11299.',
+        date: '01/08/2015',
         fileUrl: 'publications/asdf2.pdf',
-        description: '<b><a href="http://www.google.com">google</a></b>'
+    },
+    {
+        header: 'Wright KM, DiLeo A and McDannald MA (2015). "Early adversity disrupts the adult use of aversive prediction errors to reduce fear in uncertainty." Frontiers in Behavioral Neuroscience 9:277.',
+        date: '01/08/2015',
+        fileUrl: 'publications/asdf2.pdf',
+    },
+    {
+        header: 'DiLeo A, Wright KM, Mangone E, McDannald MA (2015). "Alcohol gains access to appetitive learning through adolescent heavy drinking." Behavioral Neuroscience 129(4): 371-9.',
+        date: '01/08/2015',
+        fileUrl: 'publications/asdf2.pdf',
+    },
+    {
+        header: 'Cooch N, Stalnaker TA, Chaudry S, McDannald MA, Liu TZ, Wied H, Schoenbaum G (2015). "Orbitofrontal lesions eliminate signaling of biological significance in cue-responsive ventral striatal neurons." Nature Communications 6: 7195.',
+        date: '01/08/2015',
+        fileUrl: 'publications/asdf2.pdf',
+    },
+    {
+        header: 'McDannald MA (2015). "Serotonin: Waiting but Not Rewarding." Current Biology 25(3): R103-104.',
+        date: '01/08/2015',
+        fileUrl: 'publications/asdf2.pdf',
+    },
+    {
+        header: 'McDannald MA, Esber GR, Wegener MA, Wied H, Liu TL, Stalnaker TA, Jones JL, Trageser J and Schoenbaum G (2014). "Orbitofrontal neurons acquire responses to \'valueless\' Pavlovian cues during unblocking." eLife 10.7554/eLife.02653.',
+        date: '01/08/2014',
+        fileUrl: 'publications/asdf2.pdf',
+    },
+    {
+        header: 'Berg BA, Schoenbaum G and McDannald MA (2014). "The dorsal raphe nucleus is integral to negative prediction errors in Pavlovian fear." European Journal of Neuroscience 40: 3096-3101.',
+        date: '01/08/2014',
+        fileUrl: 'publications/asdf2.pdf',
     },
 ];
 
