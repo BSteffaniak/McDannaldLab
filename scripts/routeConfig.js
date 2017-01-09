94c9f4dc02e9e12d5203bfd6450c7e8d443bc737
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-08 16:21:01 -0800

    Updated filename cases.

---
+++ scripts/routeConfig.js
@@ -0,0 +1,23 @@
+angular.module("lab").config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/home");
+    
+    $stateProvider.state("/", {
+            redirectTo: "/home"
+        }).state("home", {
+            url: "/home",
+            templateUrl: "home.html"
+        }).state("publications", {
+            url: "/publications",
+            templateUrl: "publications.html",
+            controller: "publicationsController"
+        }).state("people", {
+            url: "/people",
+            templateUrl: "people.html"
+        }).state("resources", {
+            url: "/resources",
+            templateUrl: "resources.html"
+        }).state("contact", {
+            url: "/contact",
+            templateUrl: "contact.html"
+        });
+}]);
