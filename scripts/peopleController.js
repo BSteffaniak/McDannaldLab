524d611b823d94261d1c1bf3d42937c6eddeff3b
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-09 20:52:03 -0800

    Added people controller.

---
+++ scripts/peopleController.js
@@ -0,0 +1,26 @@
+var people = [
+    {
+        name: "Mike McDannald",
+        titles: ["Title 1", "Title n"],
+        description: "Description.... Description.... Description.... Description.... Description.... Description.... Description.... Description....",
+        email: "michael.mcdannald@bc.edu",
+        imageUrl: "content/images/mike.jpg",
+        cvUrl: "resources/people/",
+    }
+];
+
+angular.module("lab").controller("PeopleController", ["$scope", "$sce", function ($scope, $sce) {
+    $scope.people = people;
+    
+    var searchColumns = ["name", "description", "titleText"];
+    
+    $scope.search = function (input) {
+        return fuzzySearch(input, $scope.searchValue, searchColumns);
+    };
+    
+    $scope.$watch("people", function (value) {
+        value.forEach(function (p) {
+            p.titleText = p.titles.join(" ");
+        });
+    });
+}]);
