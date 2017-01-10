7bf9e4b2a7751232d70dd4cb7ab513dcf55c220c
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-09 21:49:07 -0800

    Added resource pages.

--- scripts/routeConfig.js
+++ scripts/routeConfig.js
@@ -19,5 +19,17 @@ angular.module("lab").config(['$stateProvider', '$urlRouterProvider', function (
         }).state("contact", {
             url: "/contact",
             templateUrl: "contact.html"
+        }).state("resources/behavior", {
+            url: "/resources/behavior",
+            templateUrl: "resources/behavior.html"
+        }).state("resources/analysis", {
+            url: "/resources/analysis",
+            templateUrl: "resources/analysis.html"
+        }).state("resources/ardbark", {
+            url: "/resources/ardbark",
+            templateUrl: "resources/ardbark.html"
+        }).state("resources/protocols", {
+            url: "/resources/protocols",
+            templateUrl: "resources/protocols.html"
         });
 }]);
