bc3b799c100d0ef240782402a8cc967b13901f34
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-16 03:56:23 -0800

    Added query string and hash parameters code.

--- scripts/util.js
+++ scripts/util.js
@@ -39,3 +39,48 @@ function fuzzySearch(input, searchValue, searchColumns) {
         return true;
     }
 }
+
+function getQueryParams() {
+    var queryParams = {};
+    var e,
+        a = /\+/g,  // Regex for replacing addition symbol with a space
+        r = /([^&;=]+)=?([^&;]*)/g,
+        d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+        q = window.location.search.substring(1);
+
+    while (e = r.exec(q))
+       queryParams[d(e[1])] = d(e[2]);
+
+    return queryParams;
+}
+
+function getQueryString(params) {
+    var url = "";
+    
+    var index = 0;
+    
+    Object.keys(params).forEach(function (key) {
+        var value = params[key];
+        
+        if (value) {
+            if (index++ > 0) {
+                url += "&";
+            }
+            
+            url += encodeURIComponent(key) + "=" + encodeURIComponent(value);
+        }
+    });
+    
+    return url;
+}
+
+function updateQueryParams() {
+    window.queryParams = getQueryParams();
+}
+
+updateQueryParams();
+
+window.originalPathname = window.location.pathname;
+window.originalQueryString = window.location.search;
+window.originalQueryParams = window.queryParams;
+window.originalHash = window.location.hash;
