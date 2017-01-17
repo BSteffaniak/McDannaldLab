afa1acf6fb1c20ee14455bfce370264ccd89681f
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-17 15:14:25 -0800

    Updated image to scale correctly.

--- scripts/peopleController.js
+++ scripts/peopleController.js
@@ -53,6 +53,25 @@ angular.module("lab").controller("PeopleController", ["$scope", "$sce", function
     $scope.$watch("people", function (value) {
         value.forEach(function (p) {
             p.titleText = p.titles.join(" ");
+            
+            var img = new Image();
+            
+            p.style = {
+                width: '100%'
+            };
+            
+            img.onload = function() {
+                if (this.width > this.height) {
+                    $scope.$apply(function () {
+                        p.style.width = "auto";
+                        p.style.height = '100%';
+                    });
+                }
+            };
+            
+            img.src = p.imageUrl;
+            
+            p.cvName = p.cvUrl ? p.cvUrl.substring(p.cvUrl.lastIndexOf('/') + 1) : "";
         });
-    });
+    }, false);
 }]);
