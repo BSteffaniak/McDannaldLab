34a95ca09cd1f056c6544fd576a6307e7f2100fd
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-27 16:55:01 -0800

    Added validateEmail function.

--- scripts/util.js
+++ scripts/util.js
@@ -125,3 +125,8 @@ function loadScript(url, callback) {
         head.appendChild(script);
     }
 }
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
