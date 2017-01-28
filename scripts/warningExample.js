cb1ab1184d47c1e2683c4cc163ef7f2ffc39ddac
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-27 16:54:13 -0800

    Added warning example script.

---
+++ scripts/warningExample.js
@@ -0,0 +1,25 @@
+var warnings = [
+    {
+        header: 'DiLeo A, Wright KM and McDannald MA (2016). "Sub-second fear discrimination in rats: Adult impairment in adolescent heavy alcohol drinkers." Learning & Memory 23: 618-622.',
+        date: '01/2016',
+        fileUrl: '/content/publications/DiLeo2016.pdf',
+    },
+    {
+        header: 'Lopatina N, McDannald MA, Steyer CV, Peterson J, Sadacca BF, Cheer JF and Schoenbaum G (2016). "Medial orbitofrontal neurons preferentially signal cues predicting changes in reward during unblocking." Journal of Neuroscience 36(32): 8416-8424.',
+        fileUrl: '/content/publications/Lopatina2016.pdf',
+    },
+    {
+        header: 'Lopatina N, McDannald MA, Steyer CV, Sadacca BF, Cheer JF and Schoenbaum G (2015). "Lateral orbitofrontal neurons acquire responses to upshifted, downshifted, or blocked cues during Pavlovian unblocking." eLife 10.7554/eLife.11299.',
+        date: '01/08/2015',
+        fileUrl: '/content/publications/asdf2.pdf',
+    },
+    {
+        date: '01/08/2015',
+        fileUrl: '/content/publications/asdf2.pdf',
+    },
+    {
+        header: 'DiLeo A, Wright KM, Mangone E, McDannald MA (2015). "Alcohol gains access to appetitive learning through adolescent heavy drinking." Behavioral Neuroscience 129(4): 371-9.',
+        date: '01/08/2015',
+        fileUrl: '/content/publications/asdf2.pdf',
+    },
+];
