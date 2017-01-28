aee832dfda4d025df1fd782bec5222d37d740665
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-27 17:57:42 -0800

    Added getPosition function.

--- scripts/util.js
+++ scripts/util.js
@@ -130,3 +130,7 @@ function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
+
+function getPosition(string, subString, index, offset) {
+    return string.split(subString, index).join(subString).length;
+}
