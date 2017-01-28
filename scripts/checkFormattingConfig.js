4cb24a80e2bc4f598bd7a8146d2b29839ee97d2e
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-27 16:53:28 -0800

    Added publicationPropertyChecker.

--- scripts/checkFormattingConfig.js
+++ scripts/checkFormattingConfig.js
@@ -1,2 +1,17 @@
 window.checkFormattingFiles = ["/scripts/warningExample.js", "/scripts/errorExample.js", "/scripts/people.js", "/scripts/publications.js"];
 
+window.publicationPropertyChecker = {
+    header: {
+        type: "string",
+        optional: false
+    },
+    date: {
+        type: "date",
+        format: "MM/DD/YYYY",
+        optional: false
+    },
+    fileUrl: {
+        type: "url",
+        optional: true
+    }
+};
