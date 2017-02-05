a73fb45ef1e042e59ef8853771cc4e6571a02753
BSteffaniak <BradenSteffaniak@gmail.com>
2017-02-04 18:36:10 -0800

    Fixed cv urls

--- scripts/people.js
+++ scripts/people.js
@@ -13,7 +13,7 @@ var people = [
         description: 'Mahsa is a postdoc in the lab. She earned her BSc in General Biology at Bu-Ali Sina University. During her Masters she spent two years working in the lab of Prof. Abbas Haghparast at Neuroscience Research Center, Shahid Beheshti University of Medical Sciences. She completed her PhD at the University of Otago with Prof. Colin Brown and Prof. Brian Hyland, investigating the effects of oxytocin on morphine addiction (using Conditioned Place Preference and in vivo extracellular recording). She then moved to the Rosalind Franklin University of Medicine and Science, where she worked with Assistant Prof. Joanna Dabrowska, studying the contribution of oxytocin system to fear and anxiety-like behavior. Her current research focuses on the effects of physical early life stress on the activity of the dorsal raphe nucleus during a Pavlovian Fear Discrimination task. Here is Mahsa’s CV.',
         email: "asdfasdf",
         imageUrl: "/content/images/Mahsa.jpg",
-        cvUrl: "/content/cv/Moaddab CV.pdf",
+        cvUrl: "content/cv/Moaddab CV.pdf",
     },
     {
         name: "Rachel Zacharias",
@@ -21,7 +21,7 @@ var people = [
         description: 'Rachel is a Behavioral Neuroscience Ph.D. student in the McDannald lab. While obtaining her B.S. in Neuroscience and Psychology at Baldwin Wallace University in Ohio, she worked in the Neuroscience Lab with Dr. G. Andrew Mickley, Dr. Christopher Turner, and Dr. Brian Thomas on numerous research projects. At Baldwin Wallace, Rachel discovered her interest in fear learning and the neural circuitry underlying Post-Traumatic Stress Disorder. Having joined the McDannald lab in 2015, Rachel is currently working on projects concerning the impact of early life stress on fear discrimination in adulthood and the role of the periaqueductal gray in prediction error signaling.',
         email: "zacharir@bc.edu",
         imageUrl: "/content/images/RachelZacharias.jpg",
-        cvUrl: "/content/cv/Zacharias CV.pdf",
+        cvUrl: "content/cv/Zacharias CV.pdf",
     },
     {
         name: "Kristina Wright",
@@ -29,7 +29,7 @@ var people = [
         description: 'Kristina is a first year Ph.D. student in the Behavioral Neuroscience division of the Psychology department. She earned her B.S. in Psychology at Boston College while volunteering in Dr. Gorica Petrovich’s Neurobiology of Feeding Behavior Laboratory. Before joining the Ph.D. program, she devoted two years to opening Dr. Michael McDannald’s lab at Boston College, serving as a lab manager/research assistant. During this period, she examined the effects of early life stress and adolescent alcohol drinking on adult fear discrimination in rats. Additionally, she played a pivotal role in both set-up and custom design of in-vivo optogenetics and electrophysiology equipment presently in use in the lab. As a graduate student, Kristina is using a combination of complex behavior, in-vivo electrophysiology, and optogenetics to examine specific roles for the retrorubral field (RRF), dorsal raphe nucleus (DRN) and ventrolateral periaqueductal gray (vlPAG) in appropriate acquisition and maintenance of fear in adult rats. In the little spare time she has, Kristina enjoys classical ballet, drawing, witty cross stitching, and boat loads of coffee.',
         email: "wrightko@bc.edu",
         imageUrl: "/content/images/KristinaWright.jpeg",
-        cvUrl: "/content/cv/Wright CV.pdf",
+        cvUrl: "content/cv/Wright CV.pdf",
     },
     {
         name: "Madelyn Ray",
@@ -37,6 +37,6 @@ var people = [
         description: 'Maddie is a first year Ph.D. student in the department of Psychology. She earned her B.S. in Psychological Sciences in 2016 from Kansas State University where she worked in <a href="http://pickenslab.net">Dr. Charles Pickens’</a> behavioral neuroscience lab. At Kansas, Maddie studied the effects of voluntary ethanol consumption in go/no-go reversal learning and the role of the orbitofrontal cortex in go/no-go tasks. Currently, Maddie is working on examining how the medial and lateral orbitofrontal cortex regulate aversive prediction errors related to changes in uncertainty. She is also interested in studying how self-administration of drugs of abuse affects the ability to accurately and efficiently employ threat estimations in situations of uncertainty. In her spare time, Maddie enjoys drinking coffee, running, and exploring the city of Boston.',
         email: "madelyn.ray@bc.edu",
         imageUrl: "/content/images/MadelynRay.jpg",
-        cvUrl: "/content/cv/Ray CV.pdf",
+        cvUrl: "content/cv/Ray CV.pdf",
     }
 ];
