8c07e13bf8ad4c81e75e89d731d6eaddc58b0a66
BSteffaniak <BradenSteffaniak@gmail.com>
2017-02-10 10:28:37 -0800

    Added pages data as separate file

---
+++ scripts/pages.js
@@ -0,0 +1,40 @@
+window.pages = [
+    {
+        url: "home",
+        css: "/content/styles/home.css"
+    },
+    {
+        url: "publications",
+        css: "/content/styles/publications.css",
+        controller: "PublicationsController"
+    },
+    {
+        url: "people",
+        css: "/content/styles/people.css",
+        controller: "PeopleController"
+    },
+    {
+        url: "resources",
+        css: "/content/styles/resources.css",
+    },
+    {
+        url: "contact",
+        css: "/content/styles/contact.css"
+    },
+    {
+        url: "resources/behavior",
+        css: "/content/styles/resources.css",
+    },
+    {
+        url: "resources/analysis",
+        css: "/content/styles/resources.css",
+    },
+    {
+        url: "resources/ardbark",
+        css: "/content/styles/resources.css",
+    },
+    {
+        url: "resources/optogenetics",
+        css: "/content/styles/resources.css",
+    }
+];
--- scripts/routeConfig.js
+++ scripts/routeConfig.js
@@ -13,37 +13,6 @@ angular.module("lab").config(['$stateProvider', '$urlRouterProvider', '$location
         window.originalHash = null;
     }
     
-    var pages = [{
-            url: "home",
-            css: "/content/styles/home.css"
-        }, {
-            url: "publications",
-            css: "/content/styles/publications.css",
-            controller: "PublicationsController"
-        }, {
-            url: "people",
-            css: "/content/styles/people.css",
-            controller: "PeopleController"
-        }, {
-            url: "resources",
-            css: "/content/styles/resources.css",
-        }, {
-            url: "contact",
-            css: "/content/styles/contact.css"
-        }, {
-            url: "resources/behavior",
-            css: "/content/styles/resources.css",
-        }, {
-            url: "resources/analysis",
-            css: "/content/styles/resources.css",
-        }, {
-            url: "resources/ardbark",
-            css: "/content/styles/resources.css",
-        }, {
-            url: "resources/optogenetics",
-            css: "/content/styles/resources.css",
-        }];
-    
     $urlRouterProvider.otherwise("home");
     
     $stateProvider.state("/", {
